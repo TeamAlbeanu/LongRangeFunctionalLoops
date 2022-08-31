7320d5fbc740402909fa96d77ead36db5483106a
devon-cshl <40868640+devon-cshl@users.noreply.github.com>
2022-08-31 09:31:58 -0400

    commented and updated version

--- TwentyOdorSet/mainFunction_SVM_OdorIdentifierLargeOdorSet.m
+++ TwentyOdorSet/mainFunction_SVM_OdorIdentifierLargeOdorSet.m
@@ -6,24 +6,16 @@ load([dataPath 'MC_APC_PreMuscimol.mat'])
 temp=firingRates(:,:,:,1:3);
 
 
-% load('MC_highSNR.mat')
-bins = 0:0.2:4;
-Fs = 10;
+bins = 0:0.2:4; %Time bins: odor period is 4 s
+Fs = 10; % sampling rate in Hz
 
-CUMCUBEPre = [];
+CUMCUBEPre = []; % stores the odor responses from beginning of odor period to each time point in "bins"; dimensions: timebins X cells X odorrants X repeats
 for k = 1:length(bins)-1
     start = Fs*(10 + bins(1));
     stop = Fs*(10+bins(k+1));
-%     start = Fs*(10+bins(k));
-%     stop = start+200;
-%     bsline = nanmean(temp(:,:,2000:10000,:),3);
-%     intTemp = (nanmean(temp(:,:,start:stop,:),3) - bsline)./(bsline) ;
-%     intTemp = (nanmean(temp(:,:,start:stop,:),3) - bsline);
-
     CUMCUBEPre(k,:,:,:) = squeeze(nanmean(temp(:,:,start:stop,:),3));
-%     AONPre(k,:,:,:) = squeeze(intTemp);
 end
-CUMCUBEPre = CUMCUBEPre(:,idx1,:,:);
+
 
 %%
 KernelType = 'Poly';
@@ -41,7 +33,8 @@ for odorNum = 2:nOdors
     end
     odorNum
 end
-%%
+%% PLOTTING codes
+
 figure(101);
 subplot(2,1,2);hold on;
 imagesc(squeeze(mean(ANSWER_Pre,3)),[0 1]);colormap('jet')
